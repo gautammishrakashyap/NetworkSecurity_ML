37a56f63584e3da29a3cddf20587da3d910a1380
gautammishrakashyap <kumargautam7071@gmail.com>
2025-01-14 14:16:26 +0530

    kuch to kaam ho rha h

--- setup.py
+++ setup.py
@@ -11,11 +11,19 @@ def get_requirements()->List[str]:
             lines=file.readlines()
             for line in lines:
                 requirement=line.strip()
-                if requirement and requirement!= '-e':
+                if requirement and requirement!= '-e.':
                     requirement_lst.append(requirement)
     except FileNotFoundError:
         print("req.txt file not found")
     
     return requirement_lst
 
-print(get_requirements())
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="kashyap",
+    author_email="kaalkashyap393@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+    
+)
